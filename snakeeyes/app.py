c5c6ea298706f518f917de891df565d8ebecd467
abdelmounaim azziza <abdelmounaim.azziza@kplr.fr>
2022-11-22 16:24:30 +0100

    commenting lines of code

--- snakeeyes/app.py
+++ snakeeyes/app.py
@@ -5,13 +5,29 @@ def create_app():
     """
     This is a comment on python.This app is using the app factory pattern.
     """
+    """
+    Create an instance of the flask app.
+    """
     app = Flask(__name__, instance_relative_config=True)
+
+    """
+    Load custom application settings.
+    """
 
     app.config.from_object('config.settings')
     app.config.from_pyfile('settings.py', silent=True)
 
+    """
+    Creates a route(endpoint) that maps to the Home page app.The index function will be excecuted when someone visit this route.
+    It just renders a hello world response.flask will render the response as html content as long with a status code of 200 which means 
+    sucess.
+    """
+
     @app.route('/')
     def index():
-        return 'Hello World!'
 
+        return 'Hello World!'
+    """
+    Return the instance of the flask application.
+    """
     return app
